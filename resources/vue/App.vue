9f79e4d9a2bdd2d2ca8fade19da0736567838971
vmriverar1 <vmriverar1@gmail.com>
2023-10-15 02:35:37 -0500

    laravel vue con mix

---
+++ resources/vue/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div class="appWrapper">
+        <p>
+            {{ text }}
+        </p>
+    </div>
+    
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+// export default defineComponent({
+export default ({
+    data(){
+        return {
+            text: 'Hola mundo'
+        }
+    }
+    // setup() {
+        
+    // },
+})
+</script>
